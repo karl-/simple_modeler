2cb6f040019fbe9bed21e1a42145e28a137b5378
karlh07 <karlh07@gmail.com>
2016-10-24 09:17:17 -0400

    Improve camera controls, add grid

--- Assets/Code/Scripts/CameraControls.cs
+++ Assets/Code/Scripts/CameraControls.cs
@@ -1,24 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraControls : MonoBehaviour
+namespace Modeler
 {
-	public float lookSpeed = 15f;
-	public float moveSpeed = 15f;
-
-	float rotationX = 0.0f;
-	float rotationY = 0.0f;
-
-	void FixedUpdate()
+	/**
+	 *	Simple FPS flythrough style camera controls.
+	 *	
+	 *	Note - This uses hardcoded keycodes and is generally not robust.
+	 *	Would not recommend using for anything serious.
+	 */
+	public class CameraControls : MonoBehaviour
 	{
-		rotationX += Input.GetAxis("Mouse X") * lookSpeed;
-		rotationY += Input.GetAxis("Mouse Y") * lookSpeed;
+		const int RIGHT_MOUSE_BUTTON = 1;
 
-		rotationY = Mathf.Clamp (rotationY, -90f, 90f);
+		public float lookSpeed = 15f;
+		public float moveSpeed = 15f;
 
-		transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up) * Quaternion.AngleAxis(rotationY, Vector3.left);
+		float rotationX = 0.0f;
+		float rotationY = 0.0f;
 
-		transform.position += Input.GetAxis("Vertical") * transform.forward * moveSpeed * Time.deltaTime;
-		transform.position += Input.GetAxis("Horizontal") * transform.right * moveSpeed * Time.deltaTime;
+		private Vector2 previousMousePosition = Vector2.zero;
+
+		void Start()
+		{
+			previousMousePosition = Input.mousePosition;
+		}
+
+		void FixedUpdate()
+		{
+			// Always update mouse delta.
+			Vector2 mouseDelta = ((Vector2)Input.mousePosition) - previousMousePosition;
+			previousMousePosition = Input.mousePosition;
+
+			if(!Input.GetMouseButton(RIGHT_MOUSE_BUTTON))
+				return;
+
+			Vector2 screenSize = new Vector2(Screen.width, Screen.height);
+
+			rotationX += (mouseDelta.x / screenSize.x) * lookSpeed;
+			rotationY += (mouseDelta.y / screenSize.y) * lookSpeed;
+
+			rotationY = Mathf.Clamp (rotationY, -90f, 90f);
+
+			transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up) * Quaternion.AngleAxis(rotationY, Vector3.left);
+
+			transform.position += Input.GetAxis("Vertical") * transform.forward * moveSpeed * Time.deltaTime;
+			transform.position += Input.GetAxis("WorldVertical") * Vector3.up * moveSpeed * Time.deltaTime;
+			transform.position += Input.GetAxis("Horizontal") * transform.right * moveSpeed * Time.deltaTime;
+		}
 	}
 }
